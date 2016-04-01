2bfff62b758894d59da78b53f0ac4adb1db83052
renkou <slor68@gmail.com>
2016-03-31 20:33:37 -0500

    finished landing/created arrays

--- js/app-browserify.js
+++ js/app-browserify.js
@@ -5,6 +5,10 @@ require("es5-shim")
 require("babel/polyfill")
 
 var Promise = require('es6-promise').Promise
+var $ = require('jquery');
+var _ = require('underscore');
+var Backbone = require('backbone');
+
 // just Node?
 // var fetch = require('node-fetch')
 // Browserify?
@@ -22,4 +26,42 @@ var Promise = require('es6-promise').Promise
     // start app
     // new Router()
 // }
+
+/////////////donuts//////////////////
+var glazedAndIcedArray = ['Glazed', 'Chocolate Iced', 'Chocolate Sprinkle', 'Chocolate Coconut', 'Chocolate Nut', 'Strawberry Iced', 'Strawberry Sprinkle', 'White Iced', 'White Sprinkle', 'White Nut', 'White Coconut', 'Maple Iced', 'Plain Sugar', 'Cinnamon Sugar', 'Powdered Sugar', 'Crumb'];
+var vanillaCakeArray = ['Plain', 'Blueberry', 'Blueberry Glazed', 'Glazed', 'Chocolate Iced', 'Chocolate Sprinkle', 'Chocolate Coconut', 'Chocolate Nut', 'Strawberry Iced', 'Strawberry Sprinkle', 'White Iced', 'White Sprinkle', 'White Nut', 'White Coconut', 'Plain Sugar', 'Cinnamon Sugar'];
+var buttermilkArray = ['Plain', 'Glazed', 'Chocolate Iced'];
+var frenchCrullerArray = ['Glazed', 'Chocolate Iced'];
+var bigRollArray = ['Eclair', 'Cinnamon Roll', 'Cinnamon Twist', 'Apple Fritter'];
+var filledArray = ['Bavarian', 'Strawberry', 'Chocolate', 'Lemon'];
+
+
+///////////kolaches//////////////////
+var kolachesArray = ['Sausage & Cheese', 'Jumbo Sausage & Cheese', 'Plain Sausage', 'Beef Sausage', 'Jalapeño Sausage & Cheese', 'Boudain Sausage & Cheese', 'Ham & Cheese', 'Jalapeño Ham & Cheese'];
+
+///////////croissants////////////////
+var croissantsArray = ['Bacon, Egg & Cheese', 'Sausage Patty, Egg & Cheese', 'Plain', 'Sausage & Cheese', 'Jalapeño Sausage & Cheese', 'Ham & Cheese', 'Jalapeño Ham & Cheese'];
+
+///////////more items////////////////
+var moreItemsArray =  ['Sausage, Egg & Cheese Biscuit', 'Cream Cheese Danish', 'Donut Holes', 'Community Coffee','Cappachinos'];
+var burritosArray = ['Sausage, Egg & Cheese', 'Bacon, Egg & Cheese', 'Potato, Egg & Cheese'];
+var muffinsArray = ['Blueberry', 'Banana', 'Chocolate'];
+
+/////////////////////////////////////////////////
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
